55707ddd5381cc80baa5003a1abcdc60f0fe84b4
Andrew Ribeiro <andrewnetwork@gmail.com>
2019-09-22 12:56:10 -0400

    Checking operators for algebraic properties. (pg 21)

--- code/src/Chapter2.hs
+++ code/src/Chapter2.hs
@@ -29,12 +29,42 @@ createAllOps ls = do
 -- >>> (\(x,y) -> (createAllOps "ab" !! 5) x y) <$> [('a','a'),('a','b'),('b','a'),('b','b')]
 -- "abab"
 
-applyOp :: (Eq a) => [a] -> Int -> [a]
-applyOp ls n = uncurry fn <$> cartesianProd ls ls
-               where fn = createAllOps ls !! n
--- >>> applyOp "ab" 5 
--- "abab"
--- >>> applyOp "ab" <$> [0..15]
--- ["aaaa","aaab","aaba","aabb","abaa","abab","abba","abbb",
---  "baaa","baab","baba","babb","bbaa","bbab","bbba","bbbb"]
+nOp :: (Eq a) => [a] -> Int -> [((a,a),a)]
+nOp ls n = (\(x,y)-> ((x,y),fn x y)) <$> cartesianProd ls ls
+           where fn = createAllOps ls !! n
+-- >>> nOp "ab" 5 
+-- [(('a','a'),'a'),(('a','b'),'b'),(('b','a'),'a'),(('b','b'),'b')]
 
+------  Checking operators for algebraic properties. (pg 21) ---------
+-- Commutative 
+isCommutative :: (Eq a) => [a] -> (a -> a -> a) -> Bool 
+isCommutative ls fn = (uncurry fn <$> domain) == (uncurry (flip fn) <$> domain)
+                      where domain = cartesianProd ls ls 
+-- >>> nOp "ab" 0
+-- [(('a','a'),'a'),(('a','b'),'a'),(('b','a'),'a'),(('b','b'),'a')]
+-- >>> isCommutative "ab" (createAllOps "ab" !! 0)
+-- True 
+-- >>> nOp "ab" 2
+-- [(('a','a'),'a'),(('a','b'),'a'),(('b','a'),'b'),(('b','b'),'a')]
+-- >>> isCommutative "ab" (createAllOps "ab" !! 2)
+-- False 
+
+-- ★ How many operators on the "ab" set are commutative? 
+-- >>> sum $ fromEnum . isCommutative "ab" <$> createAllOps "ab"
+-- 8 
+-- Observation: Half of the operators defined on "ab" are commutative. (there are 16 in total)
+
+-- Associative 
+isAssociative ls fn = (fn <$> ls <*> (uncurry fn <$> domain)) == (fn <$> (uncurry fn <$> domain) <*> ls) 
+                      where domain = cartesianProd ls ls 
+-- >>> isAssociative "ab" (createAllOps "ab" !! 0)
+-- True 
+
+-- ★ How many operators on the "ab" set are associative? 
+-- >>> sum $ fromEnum . isAssociative "ab" <$> createAllOps "ab"
+-- 8 
+-- Observation: Half of the operators defined on "ab" are associative. 
+-- this observation does doesn't hold for all sets, i.e., like on "abc."
+
+-- TODO: Make a function to find the identity element for an operator. 
+-- TODO: Make a function which returns the inverse function for an operator. 
