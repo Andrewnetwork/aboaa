fe5ce61efd9bf4ac4b54d4d7a5e6e197c4a9003f
Andrew Ribeiro <andrewnetwork@gmail.com>
2019-09-23 02:21:05 -0400

    Started NamedValue type.

--- code/src/Chapter3.hs
+++ code/src/Chapter3.hs
@@ -1,5 +1,7 @@
 module Chapter3 where
 import Text.Printf
+import Chapter2 
+import Data.List 
 
 integerModulo :: Integral a => a -> a -> a -> a
 integerModulo n x y = mod (x+y) n 
@@ -8,22 +10,74 @@ integerModulo n x y = mod (x+y) n
 -- >>> (integerModulo 6 0) <$> [0..12]
 -- [0,1,2,3,4,5,0,1,2,3,4,5,0]
 
---operationTable :: (a -> a -> a) -> a -> String
-operationMatrix :: (t -> t -> a) -> [t] -> [[a]]
-operationMatrix op domain = listToMat (length domain) (length domain) [ op x y | x <- domain, y <- domain]
-
--- TODO: Clean up. 
-printOperationMatrix
-  :: (Text.Printf.IsChar a1, Text.Printf.PrintfArg t,
-      Text.Printf.PrintfArg a2) =>
-     (t -> t -> a2) -> [t] -> a1 -> IO ()
-printOperationMatrix op domain opSymbol = 
-    putStrLn $ printf "%3s " [opSymbol] ++ " | " ++ (domain >>= printf "%4d" ) ++ "\n" ++
-        replicate 32 '-' ++ "\n" ++ (zip domain (operationMatrix op domain) >>= 
-            (\(d,x) -> printf "%3d " d ++ " | " ++ (x >>= printf "%4d") ++ "\n"))
--- putStrLn $ operationTable (integerModulo 6) [0..5]
--- printOperationMatrix (integerModulo 6) [0..5] '+' 
-
+listToMat :: (Eq t, Num t, Enum t) => t -> Int -> [a] -> [[a]] 
 listToMat 0 _ _ = []
 listToMat _ _ [] = [] 
 listToMat nRows nCols ls = take nCols ls : listToMat (pred nRows) nCols (drop nCols ls) 
+-- >>> listToMat 5 5 (cycle [1,2,3])
+-- [[1,2,3,1,2],[3,1,2,3,1],[2,3,1,2,3],[1,2,3,1,2],[3,1,2,3,1]]  
+
+operationTable :: (t -> t -> a) -> [t] -> [[a]]
+operationTable op domain = listToMat (length domain) (length domain) [ op x y | x <- domain, y <- domain]
+-- >>> operationTable (integerModulo 6) [0..5]
+-- [[0,1,2,3,4,5],[1,2,3,4,5,0],[2,3,4,5,0,1],[3,4,5,0,1,2],[4,5,0,1,2,3],[5,0,1,2,3,4]]
+
+-- TODO: Clean up. 
+
+printOperationTable table domain opSymbol = 
+    putStrLn $ printf "%3s " [opSymbol] ++ " | " ++ (domain >>= printf "%4d" ) ++ "\n" ++
+        replicate (4*length domain+8) '-' ++ "\n" ++ (zip domain table >>= 
+            (\(d,x) -> printf "%3d " d ++ " | " ++ (x >>= printf "%4d") ++ "\n"))
+-- >>> printOperationTable (operationTable (integerModulo 6) [0..5]) [0..5] '+' 
+{-
+  +  |    0   1   2   3   4   5
+--------------------------------
+  0  |    0   1   2   3   4   5
+  1  |    1   2   3   4   5   0
+  2  |    2   3   4   5   0   1
+  3  |    3   4   5   0   1   2
+  4  |    4   5   0   1   2   3
+  5  |    5   0   1   2   3   4
+-}
+
+-- Aside: We can now use this to create truth tables for boolean operators. 
+-- >>> printOperationTable (operationTable (createAllOps [0,1] !! 6) [0,1]) [0,1] '*'
+{- XOR 
+  *  |    0   1
+----------------
+  0  |    0   1
+  1  |    1   0
+-}
+   
+dotProduct = (sum .) . zipWith (*)
+-- >>> dotProduct [1,3,-5] [4,-2,-1]
+
+matMul m1 m2 = listToMat (length m1) (length m2) [ dotProduct x y | x <- m1, y <- transpose m2]
+-- matMul [[1,0],[0,1]] [[0,1],[1,0]]
+
+data NamedValue a = NamedValue{name :: String, value :: a} 
+
+-- Page 28.
+matSet = [ NamedValue "I" [[1,0],[0,1]],   NamedValue "A" [[0,1],[1,0]],
+           NamedValue "B" [[0,1],[-1,-1]], NamedValue "C" [[-1,-1],[0,1]],
+           NamedValue "D" [[-1,-1],[1,0]], NamedValue "K" [[1,0],[-1,-1]]]
+
+instance Eq a => Eq (NamedValue a) where
+    -- We ignore names when testing for equality. 
+    (NamedValue n1 v1) == (NamedValue n2 v2) = v1 == v2
+    (NamedValue n1 v1) /=  (NamedValue n2 v2) = v1 /= v2 
+
+instance (Show a) => Show (NamedValue a) where
+    show (NamedValue n v) = n ++ " = " ++ show v
+
+instance Functor NamedValue where
+    fmap fn (NamedValue n v) = NamedValue n (fn v)
+
+findName :: Eq a => a -> [NamedValue a] -> Maybe String 
+findName _ [] = Nothing
+findName needle (NamedValue n v : xs) | needle == v = Just n
+                                      | otherwise = findName needle xs
+-- >>> findName [[1,0],[-1,-1]] matSet
+-- Just "K"
+
+-- >>> printOperationTable (operationTable matMul bareMatSet) ["I","A","B","C","D","K"] '+' 
